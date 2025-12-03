8b5c739fcf8431d27ee8d9fb41985070cb056779
tharunShiv <taunchan@MacBook.local>
2025-12-03 15:29:34 +0530

    ghpages

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js (The definitive fix)
-
-module.exports = {
-  plugins: [
-    // 💡 Use the dedicated PostCSS plugin package
-    require("@tailwindcss/postcss"),
-    require("autoprefixer"),
-  ],
-};
